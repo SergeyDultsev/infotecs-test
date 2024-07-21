f345a1c3a2f715adb7b70e23f306920459b69d3c
SergeyDultsev <121752321+Dultsev2004@users.noreply.github.com>
2024-07-21 11:29:54 +0700

    edit base in vite.config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/infotecs-test-project.io/',
+  base: '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
