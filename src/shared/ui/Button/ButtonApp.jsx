9738929f9c21d2041323963b4d552a84b1aac7aa
SergeyDultsev <121752321+Dultsev2004@users.noreply.github.com>
2024-07-21 11:03:59 +0700

    Updates

--- src/shared/ui/Button/ButtonApp.jsx
+++
@@ -1,9 +0,0 @@
-import ButtonStyle from './Button.module.scss'
-
-export function ButtonApp({ onClick, text }) {
-    return (
-        <button className={ButtonStyle['btn__default']} onClick={onClick}>
-            {text}
-        </button>
-    );
-}
